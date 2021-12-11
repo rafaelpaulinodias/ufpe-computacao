306b677b903980c5fb63315ef39350db2b439014
Rafael Dias <rafael.paulinodias@gmail.com>
2021-12-11 15:08:36 -0300

    added the problem statement

--- lista1/nadador.c
+++ lista1/nadador.c
@@ -1,3 +1,31 @@
+/*
+Categorias de nadadores
+
+Escreva um programa que, dada a idade de um nadador, classifique-o em uma das seguintes categorias:
+
+Infantil A: [5; 7]
+Infantil B: [8; 10]
+Juvenil A: [11;13]
+Juvenil B: [14;17]
+Adulto: [18;40]
+Master: A partir de 41 e sem limite superior
+Caso a idade informada esteja fora dos limites, emita uma mensagem de erro: "Idade invalida."
+
+entrada: A idade do nadador (int)
+
+saída:  A categoria em que ele deve nadar de acordo com a tabela: do enunciado
+
+ ------------------------------------------
+| Exemplo de entrada  |   Exemplo de saída |
+ ------------------------------------------
+| 6	                  |     Infantil A     |
+ ------------------------------------------
+| 11	              |     Juvenil A      |
+ ------------------------------------------
+
+Autor(a): andre.tiba@ufpe.br
+*/
+
 #include <stdio.h>
 
 int main()
